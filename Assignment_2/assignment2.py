88bebe731f88a2ed11c404654ffbb06d1bc101ea
Joos Akkerman <akkermanjoos@gmail.com>
2020-11-27 18:12:19 +0100

    Added experiment for priority and non-priority

--- Assignment_2/assignment2.py
+++ Assignment_2/assignment2.py
@@ -6,12 +6,14 @@ import plot
 
 class Client():
 
-    def __init__(self, id, env, bcs, time_of_arr, time_in_service):
+    def __init__(self, id, env, bcs, prior, time_of_arr, time_in_service):
         self.id  = id
         self.env = env
         self.bcs = bcs
         self.time_of_arr = time_of_arr
         self.time_in_service = time_in_service
+        self.key = time_in_service
+        self.prior = prior
 
         self.waiting_time = 0
 
@@ -22,17 +24,29 @@ class Client():
         # print(self.id, 'started waiting at', self.env.now)
 
         # wait until place is ready, then get service
-        with self.bcs.request() as req:
-            yield req
+        if not self.prior:
+            with self.bcs.request() as req:
+                yield req
 
-            self.waiting_time = self.env.now - self.time_of_arr
+                self.waiting_time = self.env.now - self.time_of_arr
 
-            # print(self.id, 'get serviced at', self.env.now)
-            yield self.env.timeout(self.time_in_service)
+                # print(self.id, 'get serviced at', self.env.now)
+                yield self.env.timeout(self.time_in_service)
 
-            # print(self.id, 'left at', self.env.now)
+                # print(self.id, 'left at', self.env.now)
+        else:
+            with self.bcs.request(priority=self.prior) as req:
+                yield req
 
-def experiment(n_clients, n_servers, max_time, arr_time, ser_time):
+                self.waiting_time = self.env.now - self.time_of_arr
+
+                # print(self.id, 'service time', self.time_in_service)
+                # print(self.id, 'get serviced at', self.env.now)
+                yield self.env.timeout(self.time_in_service)
+
+                # print(self.id, 'left at', self.env.now)
+
+def experiment1(n_clients, n_servers, max_time, arr_time, ser_time, sorted):
 
     clients = []
     time_of_arr = 0
@@ -43,9 +57,12 @@ def experiment(n_clients, n_servers, max_time, arr_time, ser_time):
 
     env = simpy.Environment()
     bcs = simpy.Resource(env, capacity=n_servers)
-
+    if sorted:
+        bcs = simpy.PriorityResource(env)
+        # prior = simpy.resources.resource.PriorityRequest(bcs)
+    
     for i,client in enumerate(clients):
-        c = Client(i+1, env, bcs, client[0], client[1])
+        c = Client(i+1, env, bcs, client[1], client[0], client[1])
         env.process(c.service())
         clients[i] += [c]
 
@@ -55,43 +72,19 @@ def experiment(n_clients, n_servers, max_time, arr_time, ser_time):
     return waiting_times
 
 
-def print_to_csv(results):
+def print_to_csv(results, n_experiments, type, prior):
     """
     Prints results to csv
     """
     
-    for i in results:
+    for i,d in enumerate(results):
         string = str(i)+','
-        for i,c in enumerate(results[i][1]):
+        for j,c in enumerate(results[i]):
             string += str(c)+','
 
-        f = open('res_n_servers.csv', "a")
+        f = open('res_n_servers_'+str(n_experiments)+'_'+str(type)+'_'+prior+'.csv', "a")
         f.write(string + '\n')
         f.close()
-
-def plot_and_test():
-    """
-    Plots results for different n servers and performs statistical test
-    """
-    # open results
-
-    # fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=[10,4])
-    # ax1.hist(results[1][1], label='1 server', alpha=0.5, bins=25)
-    # ax2.hist(results[2][1], label='2 servers', alpha=0.5, bins=25)
-    # ax3.hist(results[4][1], label='4 servers', alpha=0.5, bins=25)
-    # # plt.legend()
-    
-    # ax1.set_ylabel('number of experiments')
-    # ax1.set_xlabel('average waiting time')
-    # ax1.set_title('1 server')
-    # ax2.set_xlabel('average waiting time')
-    # ax2.set_title('2 servers')
-    # ax3.set_xlabel('average waiting time')
-    # ax3.set_title('4 servers')
-
-    # plt.tight_layout()
-    # plt.savefig('hist_servers_apart.pdf')
-
 
 
 
@@ -102,6 +95,7 @@ if __name__ == '__main__':
     max_time        = 500
     arr_time        = 1
     ser_time        = 0.7
+    sorted = False
 
     # rate of arival lambda
     # lamb = 10
@@ -110,16 +104,42 @@ if __name__ == '__main__':
     # # system load rho
     # rho  = lamb/(mu*n_servers)
 
-    results = {i:[] for i in [1,2,4]}
-    for n_servers in [1,2,4]:
+    all_n_experiments = np.array([int(i) for i in np.logspace(1,4,10)])
+
+    # for n_experiments in all_n_experiments:
+    #     print(n_experiments)
+    #     # if n_experiments != 10000:
+
+    #     results = {i:[] for i in [1,2,4]}
+    #     for n_servers in [1,2,4]:
+    #         all_waiting_times = []
+    #         for i in range(n_experiments):
+    #             all_waiting_times += [experiment1(n_clients, n_servers, max_time, arr_time*n_servers, ser_time, sorted)]
+    #         results[n_servers] = [all_waiting_times, [np.average(i) for i in all_waiting_times]]
+
+    #     print(results)
+
+    #     print_to_csv(results, n_experiments, 1, 'non_prior')
+    #     print_to_csv(results, n_experiments, 0, 'non_prior')
+
+        # plot.plot_nservers()
+
+    sorted = True
+    n_servers = 1
+    experiment1(n_clients, n_servers, max_time, arr_time, ser_time, sorted)
+
+    for n_experiments in all_n_experiments:
+        print(n_experiments)
+
+        results = []
+        # for n_servers in [1,2,4]:
         all_waiting_times = []
         for i in range(n_experiments):
-            all_waiting_times += [experiment(n_clients, n_servers, max_time, arr_time*n_servers, ser_time)]
-        results[n_servers] = [all_waiting_times, [np.average(i) for i in all_waiting_times]]
+            all_waiting_times += [experiment1(n_clients, n_servers, max_time, arr_time*n_servers, ser_time, sorted)]
+        results += [all_waiting_times, [np.average(i) for i in all_waiting_times]]
 
-    print(results)
+        print(results)
 
-    print_to_csv(results)
-
-    plot.plot_nservers()
+        print_to_csv(results, n_experiments, 1, 'prior')
+        print_to_csv(results, n_experiments, 0, 'prior')
 
--- Assignment_2/plot.py
+++ Assignment_2/plot.py
@@ -36,5 +36,133 @@ def plot_nservers():
     # plt.show()
     plt.savefig('hist_servers_together.pdf')
 
+
+def plot_conf_n_servers():
+
+    for type in [0,1]:
+
+        fig = plt.subplots(1, 1, figsize=[8,4])
+
+        all_n_experiments = np.array([int(i) for i in np.logspace(1,4,10)])
+
+        avgs = [[] for i in range(1,4)]
+        stds = [[] for i in range(1,4)]
+
+        for n_experiments in all_n_experiments:
+            with open('res_n_servers_'+str(n_experiments)+'_'+str(type)+'_.csv', newline='\n') as csvfile:
+                reader = csv.reader(csvfile, delimiter=',')
+
+                for i,row in enumerate(reader):
+                    row = [np.single(i) for i in row if i != '' if '[' not in i and ']' not in i]
+
+                    avgs[i] += [np.average(row)]
+                    stds[i] += [np.std(row)]
+
+        avgs = [np.array(avg) for avg in avgs]
+        stds = [np.array(std) for std in stds]
+
+        for i in range(0,3):
+            avg = avgs[i]
+            std = stds[i]
+            c = 'blue'
+            lab = '1 server'
+            if i==1:
+                c = 'red'
+                lab = '2 servers'
+            elif i==2:
+                c = 'green'
+                lab = '4 servers'
+
+            plt.plot(all_n_experiments, avg - (1.96*std)/np.sqrt(all_n_experiments),
+                    label=lab, color=c)
+            plt.plot(all_n_experiments, avg + (1.96*std)/np.sqrt(all_n_experiments),
+                    color=c)
+
+            plt.fill_between(all_n_experiments, avg - (1.96*std)/np.sqrt(all_n_experiments),
+                            avg + (1.96*std)/np.sqrt(all_n_experiments), color=c, 
+                            alpha=0.4)
+        
+        plt.xscale('log')
+        plt.xlabel('number of experiments', fontsize=14)
+        plt.ylabel('confidence interval', fontsize=14)
+        plt.legend(fontsize=12, loc='right')
+        # plt.show()
+        plt.tight_layout()
+        plt.savefig('CI_nservers_all_'+str(type)+'.pdf')
+
+
+def plot_conf_prior():
+
+    # for type in [0,1]:
+
+    type = 1
+
+    fig = plt.subplots(1, 1, figsize=[8,4])
+
+    all_n_experiments = np.array([int(i) for i in np.logspace(1,4,10)])
+
+    avgs = [[] for i in range(2)]
+    stds = [[] for i in range(2)]
+
+    for j,prior in enumerate(['', 'prior']):
+        for n_experiments in all_n_experiments:
+            with open('res_n_servers_'+str(n_experiments)+'_'+str(type)+'_'+prior+'.csv', newline='\n') as csvfile:
+                reader = csv.reader(csvfile, delimiter=',')
+
+                for i,row in enumerate(reader):
+                    if prior == '':
+                        if i==0:
+                            row = [np.single(i) for i in row if i != '' if '[' not in i and ']' not in i]
+
+                            avgs[j] += [np.average(row)]
+                            stds[j] += [np.std(row)]
+                    else:
+                        if i!=0:
+                            row = [np.single(i) for i in row if i != '' if '[' not in i and ']' not in i]
+
+                            avgs[j] += [np.average(row)]
+                            stds[j] += [np.std(row)]
+
+    avgs = [np.array(avg) for avg in avgs]
+    stds = [np.array(std) for std in stds]
+
+    print(avgs)
+
+    for i in range(2):
+        avg = avgs[i]
+        std = stds[i]
+        c = 'blue'
+        lab = 'no priority'
+        if i==1:
+            c = 'orange'
+            lab = 'priority'
+
+        print(len(avg))
+        print(len(std))
+
+        plt.plot(all_n_experiments, avg - (1.96*std)/np.sqrt(all_n_experiments),
+                label=lab, color=c)
+        plt.plot(all_n_experiments, avg + (1.96*std)/np.sqrt(all_n_experiments),
+                color=c)
+
+        plt.fill_between(all_n_experiments, avg - (1.96*std)/np.sqrt(all_n_experiments),
+                        avg + (1.96*std)/np.sqrt(all_n_experiments), color=c, 
+                        alpha=0.4)
+    
+    plt.xscale('log')
+    plt.xlabel('number of experiments', fontsize=14)
+    plt.ylabel('confidence interval', fontsize=14)
+    plt.legend(fontsize=12, loc='right')
+    plt.tight_layout()
+    # plt.show()
+    plt.savefig('CI_prior.pdf')
+
+
+    
+
 if __name__ == '__main__':
-    plot_nservers()
+    # plot_nservers()
+
+    plot_conf_n_servers()
+
+    # plot_conf_prior()
