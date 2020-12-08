b281c03b96182856a8a371470d5bbfc822ce0fa7
guido <guidovaessen@hotmail.com>
2020-12-08 18:59:10 +0100

    updated hill climbing, added absolute error
    heb wat extra argumenten aan de functies toegevoegd  en de error bereken ik nu gwn door die van de prey en predator bij elkaar op te tellen op advies van Dongwei

--- Assignment_3/main.py
+++ Assignment_3/main.py
@@ -22,12 +22,6 @@ def open_data():
                 x += [np.double(row[1])]
                 y += [np.double(row[2])]
 
-    # plt.scatter(t,x)
-    # plt.scatter(t,y)
-    # plt.xlabel('t')
-    # plt.ylabel('x and y')
-    # plt.show()
-
     return t,x,y
 
 
@@ -51,69 +45,112 @@ def integrate(param,t,x,y):
 
     return x_val, y_val
 
-    # plt.scatter(t, x_val)
-    # plt.scatter(t, x)
-    # for i,tval in enumerate(t):
-    #     plt.arrow(tval, min(x[i], x_val[i]), 0, max(x[i],x_val[i]))
-    # # plt.scatter(t, y_val[1:])
 
-    # plt.show()
+def error(x, y, x_val, y_val, method='mean squared'):
+    '''
+    Computes the loss between data and predictions
+    x, y : data
+    x_val, y_val: predictions
+    method: which loss function to use 'mean squared' or 'absolute'
+    '''
+    if method == 'mean squared':
+        error_x = np.average([(np.array(x)-np.array(x_val))**2])
+        error_y = np.average([(np.array(y)-np.array(y_val))**2])
 
-    # print(len(x))
-    # print(len(x_val))
+    elif method == 'absolute':
+        error_x = np.mean(np.abs([np.array(x) - np.array(x_val)]))
+        error_y = np.mean(np.abs([np.array(y) - np.array(y_val)]))
 
-
-def means_sq(x,y,x_val,y_val):
-    """
-    Computes means squared error
-    """
-    error_x = np.average([(np.array(x)-np.array(x_val))**2])
-    error_y = np.average([(np.array(y)-np.array(y_val))**2])
-    return error_x, error_y
+    return error_x + error_y
 
 
-def hillclimber(t,x,y):
+def hillclimber(t,x,y, method='mean squared', plot_error=False, plot_fit=False, steps=1000, 
+                n_runs=4):
+    '''
+    t, x, y: time, predator, prey data
+    error: 'absolute'or 'mean squared', defines which loss function to use
+    plot_error: boolean, whether or not to plot the evolution of the error
+    plot_fit: boolean, whether or not to plot the final fit
+    steps: int, how often the algorithm is executed
+    n_runs: int, how often the algorithm starts again from a random point
+    ''' 
 
-    # param = [np.abs(np.random.normal(0,0.5)) for i in range(4)]
-    param = [0.3,0.3,0,3,0.3]
-    # print(param)
-    x_est, y_est = integrate(param,t,x,y)
-    error_x, error_y = means_sq(x,y,x_est,y_est)
+    # dictionary with key, value: run, (errors, params)
 
-    all_error_x = []
+    output = {}
 
-    steps = 4000
+    for run in range(n_runs):
 
-    for i in range(steps):
-        change = np.random.choice(param)
-        new_param = param
-        new_param[param.index(change)] = np.abs(change + np.random.normal(0,0.1))
-        
+        param = np.random.uniform(0.1, 3, size=4).tolist()
         x_est, y_est = integrate(param,t,x,y)
-        new_error_x, new_error_y = means_sq(x,y,x_est,y_est)
 
-        # print(new_error_x)
+        error_xy = error(x,y,x_est,y_est, method=method)
 
-        if new_error_x < error_x:
-            param = new_param
-            error_x = new_error_x
+        all_error_xy = []
 
-        all_error_x += [error_x]
+        for i in range(steps):
+            change = np.random.choice(param)
+            new_param = param
+            new_param[param.index(change)] = np.abs(change + np.random.normal(0,0.1))
+            
+            x_est, y_est = integrate(param,t,x,y)
+            new_error_xy = error(x,y,x_est,y_est, method=method)
 
-    plt.plot(range(steps), all_error_x)
-    plt.show()
 
-    fig , ax = plt.subplots(1,2)
-    ax[0].scatter(t, x_est, label='est')
-    ax[0].scatter(t, x, label='real')
-    ax[0].legend()
-    # for i,tval in enumerate(t):
-    #     plt.arrow(tval, min(x[i], x_val[i]), 0, max(x[i],x_val[i]))
-    ax[1].scatter(t, y_est, label='est')
-    ax[1].scatter(t,y, label='real')
-    ax[1].legend()
+            if new_error_xy < error_xy:
+                param = new_param
+                error_xy = new_error_xy
 
-    plt.show()
+        all_error_xy += [error_xy]
+        output[run] = [error_xy, new_param]
+
+    # select best fit
+    min_error = min([val[0] for key, val in output.items()])
+    best_param = [val[1] for key, val in output.items() if val[0] == min_error][0]
+
+    if plot_error ==  True:
+        plt.plot(range(steps), all_error_xy)
+        plt.show()
+
+    # select best fit
+    min_error = min([val[0] for key, val in output.items()])
+    best_param = [val[1] for key, val in output.items() if val[0] == min_error][0]
+    x_est, y_est = integrate(best_param,t,x,y)
+
+    if plot_fit == True:
+        fig , ax = plt.subplots(1,2, figsize=(7, 4))
+        ax[0].plot(t, x_est, label='est')
+        ax[0].plot(t, x, label='real')
+        ax[0].set_title('predator')
+        ax[0].legend()
+        # for i,tval in enumerate(t):
+        #     plt.arrow(tval, min(x[i], x_val[i]), 0, max(x[i],x_val[i]))
+        ax[1].plot(t, y_est, label='est')
+        ax[1].plot(t,y, label='real')
+        ax[1].set_title('prey')
+        ax[1].legend()
+
+        plt.show()
+
+    return best_param
+
+def SA(t, x, y, p0, error='mean squared'):
+    '''
+    Simulated annealing algorithm
+    t, x, y: time, predator, prey data
+    p0: array_like, initial parameters
+    error: 'absolute'or 'mean squared', defines which loss function to use
+    '''
+
+    # initializations
+    T0 = 100   # starting temperature
+    dT = 0.1   # step size
+    Tf = 0.1   # final temperature
+    Tc = np.random.uniform(1, 100)   # current temperature
+
+    while Tc > Tf:
+        pass
+
 
 
 if __name__ == "__main__":
@@ -128,4 +165,4 @@ if __name__ == "__main__":
     #     error_x, error_y = means_sq(x,y,x_val,y_val)
     #     print(error_x, error_y)
 
-    hillclimber(t,x,y)
+    params = hillclimber(t,x,y, plot_fit=True, n_runs=10, steps=4000)
